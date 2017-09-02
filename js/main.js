5bc341f2c23918e52bc450e5c85908fd4eba55f5
Yuriy Kalichun <yurkok12@gmail.com>
2017-09-02 14:03:15 +0300

    second

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 /**
  * Created by user on 29.08.2017.
  */
-console.log(1);
+
+$(".home-slider").owlCarousel({
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    items: 1,
+    margin: 30,
+    stagePadding: 30,
+    smartSpeed: 1000,
+    loop: true,
+    autoplay: true,
+    dots: false,
+    touchDrag: false,
+    mouseDrag: false
+});
