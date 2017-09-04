e77581e353ee80a780b571c2844594b6cbc8ddb7
Yuriy Kalichun <yurkok12@gmail.com>
2017-09-04 23:07:02 +0300

    buttons are baaadddd

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@
  * Created by user on 29.08.2017.
  */
 
-// $(".home-slider").owlCarousel({
-//     animateOut: 'fadeOut',
-//     animateIn: 'fadeIn',
-//     items: 1,
-//     margin: 30,
-//     smartSpeed: 1000,
-//     loop: true,
-//     autoplay: true,
-//     dots: false,
-//     touchDrag: false,
-//     mouseDrag: false
-// });
+$(".home-slider").owlCarousel({
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    items: 1,
+    margin: 30,
+    smartSpeed: 1000,
+    loop: true,
+    autoplay: true,
+    dots: false,
+    touchDrag: false,
+    mouseDrag: false
+});
