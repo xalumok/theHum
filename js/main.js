db783405cacceb73883d0aae42f8df46d12527cc
Yuriy Kalichun <yurkok12@gmail.com>
2017-09-12 21:43:23 +0300

    name

--- js/main.js
+++ js/main.js
@@ -14,3 +14,34 @@ $(".home-slider").owlCarousel({
     touchDrag: false,
     mouseDrag: false
 });
+
+
+$(window).on("load", function () {
+    $(".css-loader").fadeOut();
+    $(".loader").delay(400).fadeOut();
+});
+
+
+function setAnimation(elem, animationType){
+    $(elem).css("opacity", "0").addClass("animated");
+    $(elem).waypoint({
+        handler: function(dir) {
+            if (dir === 'down') {
+                $(this.element).removeClass('fadeOut').addClass(animationType).css('opacity', '1');
+            } else {
+                $(this.element).removeClass(animationType).addClass('fadeOut').css('opacity', '1');
+            }
+        },
+        offset: '100%'
+    });
+}
+
+function initAnimations(){
+    setAnimation(".adv-icon", "zoomIn");
+    setAnimation(".adv-header", "zoomIn");
+    setAnimation(".section-header", "fadeLeft");
+    setAnimation(".feature-item", "fadeIn");
+    setAnimation(".about-us-devices", "slideInLeft");
+}
+
+initAnimations();
